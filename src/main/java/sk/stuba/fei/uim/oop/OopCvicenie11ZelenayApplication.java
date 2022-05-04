46cea7f2fbed8acdc71533bc4c79d8eedaeca340
Eduard Zelenay <xzelenay@stuba.sk>
2022-05-04 20:04:14 +0200

    opravene

---
+++ src/main/java/sk/stuba/fei/uim/oop/OopCvicenie11ZelenayApplication.java
@@ -0,0 +1,13 @@
+package sk.stuba.fei.uim.oop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopCvicenie11ZelenayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OopCvicenie11ZelenayApplication.class, args);
+    }
+
+}
