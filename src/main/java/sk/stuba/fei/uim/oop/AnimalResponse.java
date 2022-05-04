daf5783f9d32a912ab670b38dfb1cb88b55a3d0c
Eduard Zelenay <xzelenay@stuba.sk>
2022-05-04 15:48:58 +0200

    pridanie getAll

---
+++ src/main/java/sk/stuba/fei/uim/oop/AnimalResponse.java
@@ -0,0 +1,13 @@
+package sk.stuba.fei.uim.oop;
+
+
+public class AnimalResponse {
+    private Long id;
+
+    private String species;
+
+    AnimalResponse(Animal a){
+        this.id= a.getId();
+        this.species=a.getSpecies();
+    }
+}
