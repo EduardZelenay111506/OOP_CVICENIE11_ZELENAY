cf7df3d75cbfa044b46308a8b8285131b5f6648e
Eduard Zelenay <xzelenay@stuba.sk>
2022-05-04 20:04:03 +0200

    opravene

---
+++ src/test/java/sk/stuba/fei/uim/oop/oop_cvicenie11_zelenay/OopCvicenie11ZelenayApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.stuba.fei.uim.oop.oop_cvicenie11_zelenay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopCvicenie11ZelenayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
